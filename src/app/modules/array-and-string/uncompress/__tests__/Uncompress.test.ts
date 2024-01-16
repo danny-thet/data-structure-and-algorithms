474dacd82f53f2304a4adb45f51362f5dcba69c3
danny-thet <thetnaingoosru@gmail.com>
2024-01-16 23:13:19 +0700

    feat: add prettierrc

--- src/app/modules/array-and-string/uncompress/__tests__/Uncompress.test.ts
+++ src/app/modules/array-and-string/uncompress/__tests__/Uncompress.test.ts
@@ -1,18 +1,18 @@
-import { uncompress } from "../uncompress";
+import { uncompress } from '../uncompress'
 
-describe("Uncompress", () => {
-	it.each`
-		compressString | expectedUncompressString
-		${"2c3a1t"}    | ${"ccaaat"}
-		${"4s2b"}      | ${"ssssbb"}
-		${"2p1o5p"}    | ${"ppoppppp"}
-		${"3n12e2z"}   | ${"nnneeeeeeeeeeeezz"}
-		${"127y"}      | ${"yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"}
-	`(
-		"should uncompress a string",
-		({ compressString, expectedUncompressString }) => {
-			const uncompressString: String = uncompress(compressString);
-			expect(uncompressString).toBe(expectedUncompressString);
-		}
-	);
-});
+describe('Uncompress', () => {
+  it.each`
+    compressString | expectedUncompressString
+    ${'2c3a1t'}    | ${'ccaaat'}
+    ${'4s2b'}      | ${'ssssbb'}
+    ${'2p1o5p'}    | ${'ppoppppp'}
+    ${'3n12e2z'}   | ${'nnneeeeeeeeeeeezz'}
+    ${'127y'}      | ${'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'}
+  `(
+    'should uncompress a string',
+    ({ compressString, expectedUncompressString }) => {
+      const uncompressString: String = uncompress(compressString)
+      expect(uncompressString).toBe(expectedUncompressString)
+    }
+  )
+})
