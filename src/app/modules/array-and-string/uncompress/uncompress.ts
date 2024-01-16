596027406aaee2e303e880eb2e36fc49f807bab3
danny-thet <thetnaingoosru@gmail.com>
2024-01-16 23:09:37 +0700

    feat: uncompress string

---
+++ src/app/modules/array-and-string/uncompress/uncompress.ts
@@ -0,0 +1,20 @@
+export const uncompress = (string: String) => {
+	const result: string[] = [];
+	let i = 0;
+	let j = 0;
+	while (i < string.length) {
+		const number = "0123456789";
+		const currentChar = string[i];
+		const isNum = number.includes(currentChar);
+		if (isNum) {
+			i++;
+		} else {
+			const time = Number(string.slice(j, i));
+			result.push(currentChar.repeat(time));
+			i++;
+			j = i;
+		}
+	}
+
+	return result.join("");
+};
